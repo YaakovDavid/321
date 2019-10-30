7485ad516e83fce291744867d5d24c8bad5ccb76
yaakov <ykvdvd@gmail.com>
2019-10-30 16:46:11 -0400

    init commit

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//   console.log('here in contact');
+
+//   $('#contact-form').submit(function(e) {
+//     var name = document.getElementById('inputName');
+//     var email = document.getElementById('inputEmail');
+//     var message = document.getElementById('inputMessage');
+
+//     if (!name.value || !email.value || !message.value) {
+//       // alertify.error('Please check your entries')
+//       return false;
+//     } else {
+//       $.ajax({
+//         method: 'POST',
+//         url: 'https://formspree.io/ykvdvd@gmail.com',
+//         data: $('#contact - form').serialize(),
+//         dataType: 'json'
+//       });
+//       e.preventDefault();
+//       $(this).get(0).reset();
+//       // alertify.success('Message sent');
+//     }
+//   })
+// })
