2d27e34f555eddd27742361b1ff6d7b1722ae381
yaakov <ykvdvd@gmail.com>
2019-11-04 16:20:17 -0500

    added media queries

---
+++ js/fa-bars.js
@@ -0,0 +1,6 @@
+$("#fa-bars").click(function () {
+  $(".phone").toggleClass("mobile-head-link");
+  $(".sec-one").toggleClass("mobile-ifram-header");
+  $("#fa-bars").toggleClass("fa-bars");
+  $("#fa-bars").toggleClass("fa-times");
+})
