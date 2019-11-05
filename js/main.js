209a862d91e4e9efd807d6873ae10c710ca45afc
yaakov <ykvdvd@gmail.com>
2019-11-05 18:26:08 -0500

    testing youtube vid

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
-// $(document).ready(function() {
-// console.log('here');
+$(document).ready(function() {
+console.log('here');
 
-//   $('.container').magnificPopup({
-//     delegate: 'a',
-//     type: 'iframe',
-//     closeBtnInside: false,
-//     closeOnContentClick: true,
-//     // tLoading: '', // remove text from preloader
+  $('.container').magnificPopup({
+    delegate: 'a',
+    type: 'iframe',
+    closeBtnInside: false,
+    closeOnContentClick: true,
+    // tLoading: '', // remove text from preloader
 
-//   });
+  });
 
 
-//   var viewport = $('.viewport'),
-//     frame = viewport.find('iframe'),
-//     frameRatio = 16 / 9;
+  // var viewport = $('.viewport'),
+  //   frame = viewport.find('iframe'),
+  //   frameRatio = 16 / 9;
 
-//   $(window).on('resize', function () {
-//     var width = viewport.outerWidth(),
-//       height = viewport.outerHeight(),
-//       ratio = width / height,
-//       targetWidth = width,
-//       targetHeight = height;
+  // $(window).on('resize', function () {
+  //   var width = viewport.outerWidth(),
+  //     height = viewport.outerHeight(),
+  //     ratio = width / height,
+  //     targetWidth = width,
+  //     targetHeight = height;
 
-//     if (ratio > frameRatio) {
-//       // viewport is wider than video
-//       // correct the height
-//       targetHeight = width / frameRatio;
-//     } else {
-//       // viewport is taller than video
-//       // correct the width
-//       targetWidth = height * frameRatio;
-//     }
+  //   if (ratio > frameRatio) {
+  //     // viewport is wider than video
+  //     // correct the height
+  //     targetHeight = width / frameRatio;
+  //   } else {
+  //     // viewport is taller than video
+  //     // correct the width
+  //     targetWidth = height * frameRatio;
+  //   }
 
-//     frame.css({
-//       width: targetWidth,
-//       height: targetHeight -200
-//     });
-//   }).trigger('resize');
-// })
+  //   frame.css({
+  //     width: targetWidth,
+  //     height: targetHeight -200
+  //   });
+  // }).trigger('resize');
+})
